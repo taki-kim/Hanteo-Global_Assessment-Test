9b426e160248ab7d3e07b645393669a13422e3a8
TeukHee <rlaxmrgml@naver.com>
2025-04-18 23:43:36 +0900

    feat: 캐러셀 프로토타입 추가

---
+++ lib/carousel-list.ts
@@ -0,0 +1,43 @@
+type HeaderNavItem = {
+  title: string;
+  link: string;
+};
+
+const headerNavList: HeaderNavItem[] = [
+  {
+    title: "차트",
+    link: "/",
+  },
+  {
+    title: "Whook",
+    link: "/whook",
+  },
+  {
+    title: "이벤트",
+    link: "/event",
+  },
+  {
+    title: "뉴스",
+    link: "/news",
+  },
+  {
+    title: "스토어",
+    link: "/store",
+  },
+  {
+    title: "충전소",
+    link: "/charge",
+  },
+
+  {
+    title: "게시판",
+    link: "/dashboard",
+  },
+
+  {
+    title: "문의사항",
+    link: "/question",
+  },
+];
+
+export default headerNavList;
