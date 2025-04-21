304e8a33d67ef9478446fef4463f01767145b04f
TeukHee <rlaxmrgml@naver.com>
2025-04-21 15:36:15 +0900

    design: 캐러셀 디자인 조정

--- lib/dummyList.ts
+++ lib/dummyList.ts
@@ -1,10 +1,34 @@
 export const bannerList = [
   {
     imageLink:
+      "https://awards.hanteo.com/_next/static/media/img_home_artdesign_2024_ko.e07529e7.png",
+    title: "한터 뮤직 어워드",
+    period: "2025. 05. 12 - 2025. 05. 13",
+    link: "https://awards.hanteo.com/?l=ko&m=false",
+    status: "진행 중",
+  },
+  {
+    imageLink:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODA3MTZfMjI5/MDAxNTMxNjczMDU0NTg3.PrBrbsctqo9ONkB_FLG61lxPdHwf5hS1guKnFSPhkhUg.N9RiDqNs13hHm_qrcQjlMZb3focjyKofmBUnkUnV6UIg.JPEG.sooniemix/QgeBPBcY_400x400.jpg?type=w800",
+    title: "세계 유일의 음악차트, 한터차트",
+    period: "",
+    link: "https://www.hanteochart.com/",
+    status: "",
+  },
+  {
+    imageLink:
+      "https://s3.ap-northeast-2.amazonaws.com/hanteoresource/hanteonews/asset/real/share_logo.jpg",
+    title: "실시간 K-pop 뉴스, 한터뉴스",
+    period: "",
+    link: "https://hanteonews.com",
+    status: "",
+  },
+  {
+    imageLink:
       "https://mblogthumb-phinf.pstatic.net/MjAyMzA0MjVfNDUg/MDAxNjgyNDE5ODE5NzU1.CD7rNeAn5qkLqiShxbB5oLKDF2A4QS9rlQJc0bKY7nYg.0THhuGww_OLE--Q8ZZMfpBwnhnFNnKBWIv-ben4Kjl4g.PNG.dbdnrl7/%EB%B4%87%EC%B9%98%EB%8D%94%EB%9D%BD_17.png?type=w800",
     title: "결속밴드 팬사인회",
-    period: "2025. 04. 22 - 2025. 04. 24",
-    link: "https://www.google.com",
+    period: "2025. 04. 21 - 2025. 04. 23",
+    link: "https://awards.hanteo.com/?l=ko&m=false",
     status: "진행 중",
   },
   {
@@ -12,31 +36,7 @@ export const bannerList = [
       "https://resources.chimhaha.net/article/1712335587074-54mrj8v7nj9.webp",
     title: "결속밴드 내한 콘서트",
     period: "2025. 04. 29 - 2025. 04. 30",
-    link: "https://www.google.com",
-    status: "진행 예정",
-  },
-  {
-    imageLink:
-      "https://www.ishamelon.com/wp-content/uploads/2024/03/image-127.png",
-    title: "결속밴드 할거 없음",
-    period: "2025. 05. 12 - 2025. 05. 13",
-    link: "https://www.google.com",
-    status: "진행 예정",
-  },
-  {
-    imageLink:
-      "https://resource.hanteochart.io/asset/chart/mainbanner/main_banner_1_ko.webp",
-    title: "세계 유일의 실시간 음악차트, 한터차트",
-    period: "2025. 05. 12 - 2025. 05. 13",
-    link: "https://www.hanteochart.com/",
-    status: "진행 중",
-  },
-  {
-    imageLink:
-      "https://awards.hanteo.com/_next/static/media/img_home_artdesign_2024_ko.e07529e7.png",
-    title: "한터 뮤직 어워드",
-    period: "2025. 05. 12 - 2025. 05. 13",
     link: "https://awards.hanteo.com/?l=ko&m=false",
-    status: "진행 중",
+    status: "진행 예정",
   },
 ];
