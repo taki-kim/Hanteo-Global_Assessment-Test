8b4bacf0a788932ebf116964780502896550d9e5
TeukHee <rlaxmrgml@naver.com>
2025-04-21 12:53:55 +0900

    feat: 차트페이지 타이틀헤더 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.scss";
 import Carousel from "@/components/carousel/carousel";
+import TitleHeader from "@/components/commons/title-header/title-header";
 import ListContainer from "@/components/list-container/list-container";
 
 export default function Home() {
   return (
     <div className={styles["wrapper"]}>
       <Carousel />
+      <TitleHeader urlParams="" />
       <ListContainer />
     </div>
   );
